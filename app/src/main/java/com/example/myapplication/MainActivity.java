f1cfd88e4d30097c902beff02b1199ed33a5ef87
Takahashi <kazuya.takahashi@fox-hound.tech>
2021-12-13 20:10:57 +0900

    じゃんけん準備

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // グーが押された時のイベント
+    public void onClickRock(View view) {
+        game(0);
+    }
+    // チョキーが押された時のイベント
+    public void onClickScissors(View view) {
+        game(1);
+    }
+    // パーが押された時のイベント
+    public void onClickPaper(View view) {
+        game(2);
+    }
+    // コンピューターの手をランデムに決定し、勝敗の判定を行う
+    private void game(int btnId) {
+        //中身の処理はこの後実装します。
     }
 }
